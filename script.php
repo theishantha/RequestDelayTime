327181536635f6df9fad503f0703889ea7f905bd
Ishantha Udara <ishanthaudara96@gmail.com>
2018-08-30 12:04:49 +0530

    updated

--- script.php
+++ script.php
@@ -22,7 +22,32 @@ if ($handle) {
             // var_dump($json_array);
             // echo $json_array["operation"];
 
-            
+
+
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "selfcareapp";
+
+          // Create connection
+            $conn = new mysqli($servername, $username, $password, $dbname);
+          // Check connection
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+            VALUES ('John', 'Doe', 'john@example.com')";
+
+            if ($conn->query($sql) === true) {
+                echo "New record created successfully";
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+
+            $conn->close();
+        
+
 
 
         }

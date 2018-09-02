bf16f68f7be071041fe18bdf4f0fb6fc1ef21b7c
Ishantha Udara <ishanthaudara96@gmail.com>
2018-09-02 08:31:20 +0530

    updated

--- copy.script.php
+++ copy.script.php
@@ -1,5 +1,6 @@
 <?php
-// Db Con
+
+// Database CONNECTION
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -7,52 +8,55 @@ $dbname = "selfcareapp";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
 
-capture
+
 $handle = @fopen("nbapi.log", "r");
 
 if ($handle) {
     while (($buffer = fgets($handle, 4096)) !== false) {
 
-            
+
         $array1 = explode("|", $buffer);
         // var_dump ($array1);
 
-    
-    if ($array1[4] == 'REQUEST') {
-        $json_array = json_decode($array1[5],true);
-        // var_dump($json_array);
-        // echo $json_array["operation"];
-        $operation = $json_array["operation"];
-        $phonenumber = $json_array["msisdn"];
 
-        // var_dump($array1);
-            
-                
-                $sql = "INSERT INTO reqdealy (dateandtime, userid, transid, operation_type, phonenumber, json_string)
+        if ($array1[4] == 'REQUEST') {
+            $json_array = json_decode($array1[5], true);
+                // var_dump($json_array);
+                // echo $json_array["operation"];
+            $operation = $json_array["operation"];
+            $phonenumber = $json_array["msisdn"];
+
+                // var_dump($array1);
+
+
+            $sql = "INSERT INTO reqdealy (dateandtime, userid, transid, operation_type, phonenumber, json_string)
                 VALUES ('$array1[0]', '$array1[2]', '$array1[3]', '$operation', '$phonenumber', '$array1[5]')";
                 // echo $sql;
-                mysqli_query($conn, $sql);
+            mysqli_query($conn, $sql);
 
-    
-        }else{
 
-                $restime = $array1[0];
+        } else {
 
-                $sql1 = "SELECT * FROM `reqdealy` WHERE `transid` = '$array1[3]'";
+            $restime = $array1[0];
+
+            $sql1 = "SELECT * FROM `reqdealy` WHERE `transid` = '$array1[3]'";
                 // echo $sql1;
-                $result = mysqli_query($conn, $sql1);
+            $result = mysqli_query($conn, $sql1);
                      
                 //echo mysql_error($result);
 
 
-                if($row = mysqli_fetch_assoc($result))
-                {
+            if ($row = mysqli_fetch_assoc($result)) {
                     // var_dump($row);
-                    $reqtime = $row['dateandtime'];
+                $reqtime = $row['dateandtime'];
                     
                     // var_dump($reqtime);
-                    $restime = $array1[0];
+                $restime = $array1[0];
 
 
                 // function timestampdiff($qw, $saw)
@@ -64,7 +68,7 @@ if ($handle) {
                 // }
                 // echo timestampdiff('1524794340', '1524803100');
 
-                    
+
 
 
                 $json_array = json_decode($array1[5], true);
@@ -72,9 +76,9 @@ if ($handle) {
                 // echo $json_array["operation"];
                 $operation = $json_array["operation"];
                 $phonenumber = $json_array["msisdn"];
-     
-                    
-                }    
+
+
+            }    
 
                 // if($array1[4] = $sql1 ){
 
@@ -92,15 +96,15 @@ if ($handle) {
 
 
 
-                }
+        }
     }
 
        // exit; 
 
-    }
-    
+}
 
-  
+
+
 
 
 
